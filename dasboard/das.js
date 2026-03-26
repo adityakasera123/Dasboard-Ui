746cadb24daae4e2a815f325e9c4d0b7ca906778
Aditya <adityakasera40@gmail.com>
2026-03-26 23:47:08 +0530

    Added Chart.js part

---
+++ dasboard/das.js
@@ -0,0 +1,39 @@
+const ctx = document.getElementById("Canvas").getContext("2d");
+
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May"],
+    datasets: [{
+      label: "Users",
+      data: [12, 19, 8, 15, 10],
+      borderRadius: 8,
+      
+      backgroundColor: [
+  "#4CAF50",
+  "#2196F3",
+  "#FF9800",
+  "#9C27B0",
+  "#F44336"
+]
+    }]
+  }
+});
+
+const pie = document.getElementById("PieChart").getContext("2d");
+
+new Chart(pie, {
+  type: "pie",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green"],
+    datasets: [{
+      data: [10, 20, 30, 40],
+      backgroundColor: [
+        "#F44336",
+        "#2196F3",
+        "#FFEB3B",
+        "#4CAF50"
+      ]
+    }]
+  }
+});
